be6b927683b3639d66df7afe76dba323ed580158
Jolicous <jl@nexplore.ch>
2019-12-03 10:17:14 +0100

    change header

--- templates/header.php
+++ templates/header.php
@@ -8,23 +8,23 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/style.css" >
 
-    <title><?= $title; ?> | Bbc MVC</title>
+    <title><?= $title; ?> | Football Tips</title>
   </head>
   <body>
 
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="/">Bbc MVC</a>
+        <a class="navbar-brand" href="/">Football Tips</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="/">Home</a>
+              <a class="nav-link" href="/">Meine Tipps</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/user">User</a>
+                <a class="nav-link" href="/user">Benutzer</a>
             </li>
           </ul>
         </div>
